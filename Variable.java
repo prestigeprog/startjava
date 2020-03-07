a2e4d3a38c81fe6b83645ce20e91f2ff4baa4116
Sergey Sokolovskii <sergunok1242@gmail.com>
2020-03-07 11:37:50 +0300

    first commit

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class Variable {	
+    public static void main(String[] args) {
+        byte b = 6;
+        short c = 9;
+        int i = 30;
+        long l = 77;
+        float f = 2.3f;
+        double d = 4.5d;
+        char ch = 's';
+        boolean hh = true;
+
+
+    }
+}
