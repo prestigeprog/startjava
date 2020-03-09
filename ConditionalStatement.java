aa6f0a9e7315ce84d927ee4afdd682093cb97f7e
presige <sergunok1242@protonmail.com>
2020-03-09 19:53:19 +0300

    ispravil DZ

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -1,32 +1,38 @@
 public class ConditionalStatement {	
     public static void main(String[] args) {
         int age = 24;
-        boolean sex = true;
-        double height = 1.92d;
-        String name = "sergo";
-        if (age>20){
+        
+        if (age > 20) {
         	System.out.println("more than 20");
         }
-        if (sex==true){
+
+
+        boolean isHeManOrWoman = true;
+       
+        if (isHeManOrWoman == true) {
         	System.out.println("man");
         }
-        if (sex==false){
+        if (isHeManOrWoman == false) {
         	System.out.println("woman");
         }
-        if ( height<1.80){
+
+
+         double height = 1.92d;
+
+        if ( height < 1.80) {
         	System.out.println("gnome");
-        }
-        else
+        } else
         	System.out.println("giant");
+		
 
 
-        if (name.startsWith("m")){
+		String name = "sergo";
+
+        if (name.startsWith("m")) {
         	System.out.println("name starts with M");
-        }
-        else if (name.startsWith("i")){
+        } else if (name.startsWith("i")) {
         	System.out.println("name starts with I");
-        }
-        else
+        } else
         	System.out.println("not I and M");
 
     }
--- Variable.java
+++ Variable.java
@@ -1,13 +1,15 @@
 public class Variable {	
     public static void main(String[] args) {
-        byte b = 6;
-        short c = 9;
-        int i = 30;
-        long l = 77;
-        float f = 2.3f;
-        double d = 4.5d;
-        char ch = 's';
-        boolean hh = true;
+        byte boobsSize = 3;
+        short lentgh = 9;
+        int feetSize = 30;
+        long heigth = 77;
+        float fork = 2.3f;
+        double diameter = 4.5d;
+        char bestLetter = 's';
+        boolean isTrue = true;
+        
+        System.out.println(boobsSize, lentgh, feetSize, heigth, fork, diameter, bestLetter, isTrue);
 
 
     }
