08e7d21b9f1c005a6a5f386fca07d97ba2683e6d
presige <sergunok1242@protonmail.com>
2020-03-10 17:33:36 +0300

    dz cycle

---
+++ Cycle.java
@@ -0,0 +1,26 @@
+public class Cycle {	
+    public static void main(String[] args) {
+       int numbers1 = 0;
+       for (int i = 0;i <= 20; i++) {
+       	System.out.println(numbers1);
+       	numbers1++;
+       }
+       int numbers2 = 6;
+       while (numbers2 >= -6) {
+       	System.out.println(numbers2);
+       	numbers2 = numbers2 - 2;
+       }
+       int numbers3 = 10;
+       int temp = 0;
+       do {
+       	if (numbers3 % 2 !=0) {
+       		temp = temp + numbers3;
+       		numbers3++;
+       	} else {
+       		numbers3++;
+       	}
+       } while (numbers3 <= 20);
+       System.out.println(temp);
+	}
+}
+
