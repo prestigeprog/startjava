ee432a1793379f881dffb4c90cd8bcb474f0ecf1
presige <sergunok1242@protonmail.com>
2020-03-12 15:18:22 +0300

    dz

--- Cycle.java
+++ Cycle.java
@@ -1,20 +1,20 @@
 public class Cycle {	
     public static void main(String[] args) {
     for (int i = 0;i <= 20; i++) {
-        System.out.println(numbers1);
+        System.out.println(i);
         }
 
     int numbers1 = 6;
     while (numbers1 >= -6) {
-        System.out.println(numbers2);
-        numbers1 = numbers2 - 2;
+        System.out.println(numbers1);
+        numbers1 = numbers1 - 2;
         }
        
     int numbers2 = 10;
     int result = 0;
     do {
         if (numbers2 % 2 !=0) {
-            result = temp + numbers2;
+            result = result + numbers2;
             numbers2++;
         } else {
             numbers2++;
