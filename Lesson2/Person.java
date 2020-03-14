b7c3aa409d54d89b90cd52bd4913e31ef9a68c9a
presige <sergunok1242@protonmail.com>
2020-03-14 16:08:54 +0300

    dz2

---
+++ Lesson2/Person.java
@@ -0,0 +1,20 @@
+public class Person {   
+    char sex = 'm';
+    String name = "RicardoMilos";
+    int height = 2;
+    int weight = 80;
+    int age = 40;
+
+    void dance() {
+    }
+    void jump() {
+    }
+    void sing() {
+    }
+    void go() {
+    }
+    void learnJava() {
+    }
+
+}
+
