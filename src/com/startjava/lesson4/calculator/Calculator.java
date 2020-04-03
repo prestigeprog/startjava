10cad6c68e96a78a33634f9edc7203499a94bbc3
presige <sergunok1242@protonmail.com>
2020-04-03 12:58:25 +0300

    dz

--- src/com/startjava/lesson4/calculator/Calculator.java
+++ src/com/startjava/lesson4/calculator/Calculator.java
@@ -3,25 +3,25 @@ package com.startjava.lesson4.calculator;
 public class Calculator {
     private String mathOperation;
 
-    public String calculate(String mathOperation) {
+    public int calculate(String mathOperation) {
         this.mathOperation = mathOperation;
         String[] splitMathOperation = mathOperation.split(" ");
         int firstNum = Integer.parseInt(splitMathOperation[0]);
         int secondNum = Integer.parseInt(splitMathOperation[2]);
         switch (splitMathOperation[1]) {
             case "+":
-                return Integer.toString(firstNum + secondNum);
+                return (firstNum + secondNum);
             case "-":
-                return Integer.toString(firstNum - secondNum);
+                return (firstNum - secondNum);
             case "*":
-                return Integer.toString(firstNum * secondNum);
+                return (firstNum * secondNum);
             case "/":
-                return Integer.toString(firstNum / secondNum);
+                return (firstNum / secondNum);
             case "%":
-                return Integer.toString(firstNum % secondNum);
+                return (firstNum % secondNum);
             case "^":
-                return Double.toString(Math.pow(firstNum, secondNum));
+                return (int) (Math.pow(firstNum, secondNum));
         }
-        return " ";
+        return 0;
     }
 }
